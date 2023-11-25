6c98bb56a62e9351bafba5a38545f32f88eee47e
sameer-manek <sameermanek@hotmail.com>
2023-11-25 19:48:54 +0530

    made site little more responsive

--- index.php
+++ index.php
@@ -1,5 +1,8 @@
 <html>
 	<head>
+		<meta charset="utf-8">
+		<meta name="viewport" content="width=device-width, initial-scale=1">
+
 		<title>Sameer Manek's blog</title>
 
 		<link rel="stylesheet" href="./css/style.css" />
