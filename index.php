9cd14af4fc9967a949707ba5d06eb6b518097775
sameer-manek <sameermanek@hotmail.com>
2023-11-25 18:25:53 +0530

    init - basic website with arcade

--- index.php
+++ index.php
@@ -10,9 +10,8 @@
 			<div class="container">
 				<h1>Sameer Manek</h1>
 				<a href="#">About</a>
-				<a href="#">Blog</a>
-				<a href="/arcade/">Arcade</a>
-				<a href="mailto:sameermanek@hotmail.com">Email</a>
+				<a href="https://github.com/sameer-manek" target="__blank">Github</a>
+				<a href="mailto:sameermanek@outlook.com">Email</a>
 			</div>
 		</header>
 
@@ -23,24 +22,26 @@
 				<div class="section">
 					<h3># Overview</h3>
 					<p>Hello, Thanks for visiting my small place on the Internet. I am a programmer who knows some stuff about building websites, games and softwares in general. I will be using this website to write some blogs, upload some fun experiments / games using HTML and share updates on my life.</p>
+					<p><b>NOTE:</b> This website is pretty much in development, expect more things coming up soon.</p>
 				</div>
 
 				<div class="section">
 					<h3># Arcade</h3>
 					<p>You will find some HTML games to play in this section. Code for these games has been open sourced on <a href="https://github.com/sameer-manek">my GitHub profile</a>.</p>
-					<ul class="grid">
+					<div class="grid">
 						<?php
 							$files = scandir("./arcade/");
 							foreach ($files as $file) {
 								if ($file != "."&& $file != "" && $file != "..") {
 									echo "
-										<a href='/arcade/$file/'>
-											<img src='/arcade/$file/thumbnail.png' alt='' width='192px' />
+										<a class='grid-item' href='/arcade/$file/'>
+											<img src='/arcade/$file/thumbnail.png' alt='' width='192px' /><br/>
+											<h5>$file</h5>
 										</a><br />";
 								}
 							}
 						?>
-					</ul>
+					</div>
 					
 				</div>
 			</div>
